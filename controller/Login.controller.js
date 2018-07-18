7d1df2a4cb075ecb60e68caf3e3751f7e06ab9e5
thxnder <rrln.1810@gmail.com>
2018-07-18 16:55:24 -0500

    ddd

--- controller/Login.controller.js
+++ controller/Login.controller.js
@@ -36,7 +36,44 @@ sap.ui.define([
 			}
 		},
 		onCorrectPathClick: function() {
-			this._oModel.setProperty("/Source", this._sValidPath);
+			function cancelZoom()
+{
+    var d = document,
+        viewport,
+        content,
+        maxScale = ',maximum-scale=',
+        maxScaleRegex = /,*maximum\-scale\=\d*\.*\d*/;
+
+    // this should be a focusable DOM Element
+    if(!this.addEventListener || !d.querySelector) {
+        return;
+    }
+
+    viewport = d.querySelector('meta[name="viewport"]');
+    content = viewport.content;
+
+    function changeViewport(event)
+    {
+        // http://nerd.vasilis.nl/prevent-ios-from-zooming-onfocus/
+        viewport.content = content + (event.type == 'blur' ? (content.match(maxScaleRegex, '') ? '' : maxScale + 10) : maxScale + 1);
+    }
+
+    // We could use DOMFocusIn here, but it's deprecated.
+    this.addEventListener('focus', changeViewport, true);
+    this.addEventListener('blur', changeViewport, false);
+}
+
+// jQuery-plugin
+(function($)
+{
+    $.fn.cancelZoom = function()
+    {
+        return this.each(cancelZoom);
+    };
+
+    // Usage:
+    $('input:text,select,textarea').cancelZoom();
+})(jQuery);
 		},
 
 		onIncorrectPathClick: function() {
