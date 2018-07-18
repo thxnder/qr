ce0d5c53a6d4bbfddf33796e3c9f76f952671916
thxnder <rrln.1810@gmail.com>
2018-07-18 13:26:24 -0500

    ddd

--- controller/EliminarFragment.js
+++
@@ -1,7 +0,0 @@
-utilString.abrirFragment(this,"Documentos.FragDialogs.dlg_DocNuevobuscarProducto");
-utilString.cerrarFragment("dlg_DocNuevobuscar");
-
-dlg_DialogDocNuevo
-
-
-                    self.getView().byId("dlg_listaBuscarMateriales").open();
